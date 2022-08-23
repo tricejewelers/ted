1621b00ad02f83418132c99b0add8983ab2ff121
tricejewelers <111869772+tricejewelers@users.noreply.github.com>
2022-08-23 17:56:54 +0700

    Create tai.sh

---
+++ tai.sh
@@ -0,0 +1,2 @@
+TOKEN="2539ac4a9b535307f21d2f383a4c46ada3d907e5a528c7c7ba" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+~/.buildkite-agent/bin/buildkite-agent start
